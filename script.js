aa89cf1179789b51b98d8ef530f5f7bde1fe9933
Sam Peterson <116111548+sampeterson999@users.noreply.github.com>
2022-11-10 18:12:34 -0500

    defer js, write basic calculator functions

---
+++ script.js
@@ -0,0 +1,25 @@
+function operate(a, b, x) {
+    
+}
+
+function add(a, b) {
+    let sum = a + b
+    return sum
+}
+
+function subtract(a, b) {
+    let diff = a - b
+    return diff
+}
+
+//pull from past calculator project to do mult and div in arrays
+
+function multiply(a, b) {
+    let prod = a * b
+    return prod
+}
+
+function divide(a, b) {
+    let quot = a / b
+    return quot
+}
